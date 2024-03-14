218de6d9b79cedb2a924810c81af543b3be95612
Eric Vantillard <eric.vantillard@evaxion.fr>
2024-03-14 17:14:25 +0100

    Extract method for background loading.

--- src/my_paint.c
+++ src/my_paint.c
@@ -19,23 +19,27 @@ void repair(sfRenderWindow* window, sfEvent event)
     }
 }
 
+static sfSprite *load_background() {
+    sfSprite *sprite = sfSprite_create();
+    sfTexture *texture = sfTexture_createFromFile("Votre texte de paragraphe.png", NULL);
+    sfSprite_setTexture(sprite, texture, sfTrue);
+    sfVector2f scale = {1, 1};
+    sfSprite_setScale(sprite, scale);
+    return sprite;
+}
+
 void fenetre()
 {
-    sfEvent event;
-    sfTexture *texture = sfTexture_createFromFile("Votre texte de paragraphe.png", NULL);
-    sfSprite *sprite = sfSprite_create();
     sfVideoMode video_mode = {1920, 1080, 32};
-    sfVector2f scale = {1, 1};
-
     sfRenderWindow *window = sfRenderWindow_create(video_mode,"#myradar", sfDefaultStyle, NULL);
-    sfSprite_setTexture(sprite, texture, sfTrue);
-    sfSprite_setScale(sprite, scale);
 
     PaintState paint_state = PAINT_STATE_IDLE;
     PaintContext *paint_context =  paint_context_create(window);
+    sfSprite* background = load_background();
 
+    sfEvent event;
     while (sfRenderWindow_isOpen(window)) {
-        sfRenderWindow_drawSprite(window, sprite, NULL);
+        sfRenderWindow_drawSprite(window, background, NULL);
         paint_context_draw_on_window(paint_context);
         sfRenderWindow_display(window);
         while (sfRenderWindow_pollEvent(window, &event)) {
@@ -44,6 +48,7 @@ void fenetre()
         }
     }
 
+    sfSprite_destroy(background);
     paint_context_destroy(paint_context);
     sfRenderWindow_destroy(window);
 }
